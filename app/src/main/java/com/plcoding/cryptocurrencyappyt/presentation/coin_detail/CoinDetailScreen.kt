5fe36715c624f60c74e92c4318062654fc827e58
Office PC <72garmartin@gmail.com>
2021-09-14 14:45:54 +0700

    finishing and change launcher logo

--- app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_detail/CoinDetailScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
@@ -22,9 +24,9 @@ import com.plcoding.cryptocurrencyappyt.presentation.Screen
 import com.plcoding.cryptocurrencyappyt.presentation.coin_detail.CoinDetailViewModel
 import com.plcoding.cryptocurrencyappyt.presentation.coin_list.components.CoinListItem
 
-@Preview
+
 @Composable
-fun CoinListScreen(
+fun CoinDetailScreen(
     viewModel: CoinDetailViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
@@ -48,21 +50,25 @@ fun CoinListScreen(
                             text = if(coin.isActive) "active" else "inactive",
                             color = if(coin.isActive) Color.Green else Color.Red,
                             fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
                             modifier = Modifier
                                 .align(CenterVertically)
                                 .weight(2f)
                         )
                     }
+
                     Spacer(modifier = Modifier.height(15.dp))
                     Text(
                         text = coin.description,
                         style = MaterialTheme.typography.body2
                     )
+
                     Spacer(modifier = Modifier.height(15.dp))
                     Text(
                         text = "Tags",
                         style = MaterialTheme.typography.h3
                     )
+
                     Spacer(modifier = Modifier.height(15.dp))
                     FlowRow(
                         mainAxisSpacing = 10.dp,
@@ -73,6 +79,20 @@ fun CoinListScreen(
                             CoinTag(tag = tag)
                         }
                     }
+
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Team Members",
+                        style = MaterialTheme.typography.h3
+                    )
+
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                items(coin.team) { teamMember ->
+                    TeamListItem(teamMember = teamMember, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp))
+                    Divider()
                 }
             }
         }
